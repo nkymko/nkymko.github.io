ff8feb4f13da0435b2b67db277c2072cd8a6a351
nkymko <nickymickoayub@gmail.com>
2024-01-22 17:52:54 +0700

    update ui

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,21 +1,27 @@
 window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('#navbar');
-    if (window.scrollY > 10) {
-        navbar.classList.add('backdrop-blur-sm', 'bg-black/20', 'border-b' , 'border-b-[#040D12]');
-    } else {
-        navbar.classList.remove('backdrop-blur-sm', 'bg-black/20', 'border-b' , 'border-b-[#040D12]');
-    }
-    
-    if (window.scrollY > 300) {
+    if (window.scrollY > 500 && window.scrollY < 1800) {
+        // document.getElementById('g-1').style.opacity = 0;
+        // document.getElementById('g-2').style.opacity = 1;
+        // document.getElementById('g-3').style.opacity = 0;
+
+        document.getElementById('my-pic').classList.remove('grayscale');
+       
+    } else if (window.scrollY > 1800) {
         document.getElementById('g-1').style.opacity = 0;
         document.getElementById('g-2').style.opacity = 1;
-       
-    } else if (window.scrollY > 500) {
+        // document.getElementById('g-3').style.opacity = 1;
 
+        document.getElementById('exp-1').classList.remove('grayscale');
+        document.getElementById('exp-2').classList.remove('grayscale');
 
     } else {
         document.getElementById('g-1').style.opacity = 1;
         document.getElementById('g-2').style.opacity = 0;
+        // document.getElementById('g-3').style.opacity = 0;
+
+        document.getElementById('my-pic').classList.add('grayscale');
+        document.getElementById('exp-1').classList.add('grayscale');
+        document.getElementById('exp-2').classList.add('grayscale');
     }
   
 });
